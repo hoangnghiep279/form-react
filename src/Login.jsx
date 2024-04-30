4953e3c2efc56cf1a8ffc3f94090522ca661227c
HOANG NGHIEP <hoangnghiep279@gmail.com>
2024-04-30 08:47:22 +0700

    add var for color

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,49 +3,58 @@ import { Link } from "react-router-dom";
 const Login = () => {
     return (
         <div>
-            <div className="w-[430px] bg-[#fff] shadow-xl rounded-xl p-[30px]">
-                <h1 className="text-[#191720] text-[26px] font-semibold leading-8">Sign In</h1>
-                <a href="#!" className="border-[#19172059] p-[11px] rounded-md border-2 flex items-center mt-[30px]">
+            <div className="w-[430px] bg-[var(--white-color)] shadow-xl rounded-xl p-[30px]">
+                <h1 className="text-[var(--text-color)] text-[26px] font-semibold leading-8">Đăng nhập</h1>
+                <a
+                    href="#!"
+                    className="border-[var(--border-color)] p-[11px] rounded-md border-2 flex items-center mt-[30px]"
+                >
                     <div className="flex items-center justify-center bg-[#0866ff] w-6 h-6 rounded-full ">
                         <img src="../src/assets/icon/facebook.svg" alt="" className="w-[10px]" />
                     </div>
-                    <span className="text-[#575363] text-[16px] grow">Continue with Facebook</span>
+                    <span className="text-[#575363] text-[16px] grow">Tiếp tục với Facebook</span>
                 </a>
-                <a href="#!" className="border-[#19172059] p-[11px] rounded-md border-2 flex items-center mt-[30px]">
+                <a
+                    href="#!"
+                    className="border-[var(--border-color)] p-[11px] rounded-md border-2 flex items-center mt-[30px]"
+                >
                     <div>
                         <img src="../src/assets/icon/google.svg" alt="" className="w-[20px]" />
                     </div>
-                    <span className="text-[#575363] text-[16px] grow">Continue with Facebook</span>
+                    <span className="text-[#575363] text-[16px] grow">Tiếp tục với Google</span>
                 </a>
-                <a href="#!" className="border-[#19172059] p-[11px] rounded-md border-2 flex items-center mt-[30px]">
+                <a
+                    href="#!"
+                    className="border-[var(--border-color)] p-[11px] rounded-md border-2 flex items-center mt-[30px]"
+                >
                     <div>
                         <img src="../src/assets/icon/apple.svg" alt="" className="w-[20px]" />
                     </div>
-                    <span className="text-[#575363] text-[16px] grow">Continue with Facebook</span>
+                    <span className="text-[#575363] text-[16px] grow">Tiếp tục với Apple</span>
                 </a>
-                <span className="uppercase text-[#575363] text-[16px] my-5 block">or</span>
+                <span className="uppercase text-[#575363] text-[16px] my-5 block">Hoặc</span>
                 <form action="">
-                    <div className="border-[#19172059] p-[15px] rounded-md border-2 flex ">
+                    <div className="border-[var(--border-color)] p-[15px] rounded-md border-2 flex ">
                         <input
                             type="text"
                             placeholder="Email"
-                            className="outline-none border-none grow text-[#191720]"
+                            className="outline-none border-none grow text-[var(--text-color)]"
                         />
                     </div>
-                    <div className="border-[#19172059] p-[15px] rounded-md border-2 flex mt-5">
+                    <div className="border-[var(--border-color)] p-[15px] rounded-md border-2 flex mt-5">
                         <input
                             type="password"
-                            placeholder="Password"
-                            className="outline-none border-none grow text-[#191720]"
+                            placeholder="Mật khẩu"
+                            className="outline-none border-none grow text-[var(--text-color)]"
                         />
                     </div>
 
-                    <button className="text-[#575363b3] text-[16px] flex ml-auto mt-3">Forgot password?</button>
-                    <button className="rounded-md bg-[#572af8] w-full p-[15px] mt-5">Sign in</button>
-                    <span className="text-[#575363b3] text-[14px] ">
-                        Don’t a have account?{" "}
+                    <button className="text-[#575363b3] text-[16px] flex ml-auto mt-3">Quên mật khẩu</button>
+                    <button className="rounded-md bg-[var(--btn-bg-color)] w-full p-[15px] mt-5">Đăng Ký</button>
+                    <span className="text-[#575363b3] text-[14px] mt-2 block">
+                        Bạn chưa có tài khoản?{" "}
                         <Link to={"Signup"} className="text-[#191720] font-semibold">
-                            Sign up
+                            Đăng ký
                         </Link>
                     </span>
                 </form>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -2,62 +2,71 @@ import { Link } from "react-router-dom";
 const Signup = () => {
     return (
         <div>
-            <div className="w-[430px] bg-[#fff] shadow-xl rounded-xl p-[20px]">
-                <h1 className="text-[#191720] text-[26px] font-semibold leading-6">Sign In</h1>
-                <a href="#!" className="border-[#19172059] p-[11px] rounded-md border-2 flex items-center mt-[15px]">
+            <div className="w-[430px] bg-[var(--white-color)] shadow-xl rounded-xl p-[20px]">
+                <h1 className="text-[var(--text-color)] text-[26px] font-semibold leading-6">Sign In</h1>
+                <a
+                    href="#!"
+                    className="border-[var(--border-color)] p-[11px] rounded-md border-2 flex items-center mt-[15px]"
+                >
                     <div className="flex items-center justify-center bg-[#0866ff] w-6 h-6 rounded-full ">
                         <img src="../src/assets/icon/facebook.svg" alt="" className="w-[10px]" />
                     </div>
-                    <span className="text-[#575363] text-[16px] grow">Continue with Facebook</span>
+                    <span className="text-[var(--text-second-color)] text-[16px] grow">Tiếp tục với Facebook</span>
                 </a>
-                <a href="#!" className="border-[#19172059] p-[11px] rounded-md border-2 flex items-center mt-[15px]">
+                <a
+                    href="#!"
+                    className="border-[var(--border-color)] p-[11px] rounded-md border-2 flex items-center mt-[15px]"
+                >
                     <div>
                         <img src="../src/assets/icon/google.svg" alt="" className="w-[20px]" />
                     </div>
-                    <span className="text-[#575363] text-[16px] grow">Continue with Facebook</span>
+                    <span className="text-[var(--text-second-color)] text-[16px] grow">Tiếp tục với Google</span>
                 </a>
-                <a href="#!" className="border-[#19172059] p-[11px] rounded-md border-2 flex items-center mt-[15px]">
+                <a
+                    href="#!"
+                    className="border-[var(--border-color)] p-[11px] rounded-md border-2 flex items-center mt-[15px]"
+                >
                     <div>
                         <img src="../src/assets/icon/apple.svg" alt="" className="w-[20px]" />
                     </div>
-                    <span className="text-[#575363] text-[16px] grow">Continue with Facebook</span>
+                    <span className="text-[var(--text-second-color)] text-[16px] grow">Tiếp tục với Apple</span>
                 </a>
-                <span className="uppercase text-[#575363] text-[16px] my-2 block">or</span>
+                <span className="uppercase text-[var(--text-second-color)] text-[16px] my-2 block">Hoặc</span>
                 <form action="">
-                    <div className="border-[#19172059] p-[15px] rounded-md border-2 flex ">
+                    <div className="border-[var(--border-color)] p-[15px] rounded-md border-2 flex ">
                         <input
                             type="text"
-                            placeholder="First name"
-                            className="outline-none border-none grow text-[#191720]"
+                            placeholder="Họ"
+                            className="outline-none border-none grow text-[var(--text-color)]"
                         />
                     </div>
-                    <div className="border-[#19172059] p-[15px] rounded-md border-2 flex  mt-3">
+                    <div className="border-[var(--border-color)] p-[15px] rounded-md border-2 flex  mt-3">
+                        <input
+                            type="text"
+                            placeholder="Tên"
+                            className="outline-none border-none grow text-[var(--text-color)]"
+                        />
+                    </div>
+                    <div className="border-[var(--border-color)] p-[15px] rounded-md border-2 flex  mt-3">
                         <input
                             type="text"
                             placeholder="Email"
-                            className="outline-none border-none grow text-[#191720]"
+                            className="outline-none border-none grow text-[var(--text-color)]"
                         />
                     </div>
-                    <div className="border-[#19172059] p-[15px] rounded-md border-2 flex  mt-3">
-                        <input
-                            type="text"
-                            placeholder="Last name"
-                            className="outline-none border-none grow text-[#191720]"
-                        />
-                    </div>
-                    <div className="border-[#19172059] p-[15px] rounded-md border-2 flex mt-3">
+                    <div className="border-[var(--border-color)] p-[15px] rounded-md border-2 flex mt-3">
                         <input
                             type="password"
-                            placeholder="Password"
-                            className="outline-none border-none grow text-[#191720]"
+                            placeholder="Mật khẩu"
+                            className="outline-none border-none grow text-[var(--text-color)]"
                         />
                     </div>
 
-                    <button className="rounded-md bg-[#572af8] w-full p-[15px] mt-7">Sign in</button>
-                    <span className="text-[#575363b3] text-[14px] ">
-                        Have an account?
-                        <Link to={"/"} className="text-[#191720] font-semibold">
-                            Sign In
+                    <button className="rounded-md bg-[var(--btn-bg-color)] w-full p-[15px] mt-7">Đăng Nhập</button>
+                    <span className="text-[#575363b3] text-[14px] mt-2 block ">
+                        Bạn đã có tài khoản?
+                        <Link to={"/"} className="text-[var(--text-color)] font-semibold">
+                            Đăng nhập
                         </Link>
                     </span>
                 </form>
